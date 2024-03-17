38d70b35d79fee7c74e57ec21d03bb3d98742498
Harsh-Choudhary-Dev <harshchoudhary50226@gmail.com>
2024-03-18 00:00:25 +0530

    initial comit

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ const express = require('express');
 const pdfParse = require('pdf-parse');
 const app = express();
 import getVectorStores from "./googleAI.js";
-const port = 80;
+const port = 3000;
 
-const __dirname = path.resolve();
+
 app.get('/', async(req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.resolve('public\\index.html'));
 });
 
 
