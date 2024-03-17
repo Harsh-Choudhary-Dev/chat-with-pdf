e639c642f608ec9ca7b17f5b686b5f9fbfdb3c4c
Harsh Choudhary <harshchoudhary50226@gmail.com>
2024-03-17 20:09:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const express = require('express');
 const pdfParse = require('pdf-parse');
 const app = express();
 import getVectorStores from "./googleAI.js";
-const port = process.env.PORT;
+const port = 3000;
 app.use(express.static("public"))
 const storage = multer.memoryStorage();
 const upload = multer({ dest: "uploads/" }); // Specify a directory for temporary storage
